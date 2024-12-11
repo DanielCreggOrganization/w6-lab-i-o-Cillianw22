43593a5a8dd759cdabdf9cbbbeb6b93f1598b628
Cillianw22 <114486302+Cillianw22@users.noreply.github.com>
2024-12-11 20:22:05 +0000

    changes to main + classes

--- src/ie/atu/iolab/Main.java
+++ src/ie/atu/iolab/Main.java
@@ -1,8 +1,21 @@
 package ie.atu.iolab;
 
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Java I/O!");
+
+        // Placeholder for initializing the VideoGame Manager
+        VideoGameManager videoGameManager = new VideoGameManager();
+        
+        // Example: Add a video game (to test the setup)
+        videoGameManager.addVideoGame(
+                new VideoGame("Minecraft", "Sandbox", 2011, 9.0, "Mojang")
+        );
+
+        // Example: List all video games
+        System.out.println("List of Video Games:");
+        videoGameManager.listAllGames();
     }
 }
